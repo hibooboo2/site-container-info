6238f5dff7b2bf1cb23d8b3f9ba60615bde7dbd6
James Harris <james@rancher.com>
2015-04-19 23:24:06 -0700

    Use a table now.

--- server.js
+++ server.js
@@ -2,32 +2,47 @@ var Docker = require('dockerode');
 var docker = Docker();
 var moment = require('moment');
 
-var containerToRow = function(container){
-	
+var jsonToRow =  function(server){
+	row = "<tr>";
+		row += "<td><a href=\"https://" + server.name + "\">" + server.name + "</a></td>";
+		row += "<td>Container name= " + server.id+ "</td>";
+	row += "</tr>";
+	return row;
+}
+
+var rowsToTable = function(rows){
+	table = "<table style=\"width:40em\">";
+	table += "<tr><th>Site </th><th>Container Id on Server</th></tr>";
+	rows.forEach(function(row){
+		table += row;
+	})
+	table += "</table>"
+	return table;
+}
+
+var serverIdJson = function(container, res, checked, stop){
+	checked += 1;
+		container.Config.Env.forEach(function(env){
+			env = env.split('=');
+			if ("VIRTUAL_HOST" === env[0]){
+				server = {"name": env[1], "id": data.Id};
+				servers.push(jsonToRow(server));
+			}
+		});
+		if (checked == stop){
+			res.end(rowsToTable(servers));
+			console.log(moment().toLocaleString() + '  ' + servers);
+		}
 }
 
 var allServers = function (res, containers){
 	var servers = [];
 	var checked = 0;
 	containers.forEach(function (containerInfo){
-		docker.getContainer(containerInfo.Id).inspect(function(err, data){
-			checked += 1;
-			data.Config.Env.forEach(function(env){
-				env = env.split('=');
-				if ("VIRTUAL_HOST" === env[0]){
-					server = {"name": env[1], "id": data.Id}
-					servers.push(server)"<a href=\"https://" + env[1] + "\">" + env[1] + "</a> Container name= " + data.Id + "<hr/>");
-				}
-			});
-			console.log(servers);
-			if (checked == containers.length){
-				responseText = "";
-				res.end(servers.toString());
-				console.log(moment().toLocaleString() + '  ' + servers);
-			}
+		docker.getContainer(containerInfo.Id).inspect(function(err, container){
+			serverIdJson(container, res, checked, stop);
 		});
 	});
-	return servers;
 }
 
 var http = require('http');
