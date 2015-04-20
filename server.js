51aab999502d871248850a25af0a8abf8d001e45
James Harris <wizardofmath@gmail.com>
2015-04-19 23:46:50 -0700

    Table now works.

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var moment = require('moment');
 var jsonToRow =  function(server){
 	row = "<tr>";
 		row += "<td><a href=\"https://" + server.name + "\">" + server.name + "</a></td>";
-		row += "<td>Container name= " + server.id+ "</td>";
+		row += "<td>" + server.id + "</td>";
 	row += "</tr>";
 	return row;
 }
 
 var rowsToTable = function(rows){
-	table = "<table style=\"width:40em\">";
+	table = "<style>table, th, td {    border: 1px solid black;    border-collapse: collapse;}th, td {    padding: 5px;    text-align: left;}table#t01 {    width: 100%;        background-color: #f1f1c1;}</style>";
+	table += "<table style=\"width:40em;border:1px\">";
 	table += "<tr><th>Site </th><th>Container Id on Server</th></tr>";
 	rows.forEach(function(row){
 		table += row;
@@ -20,27 +21,27 @@ var rowsToTable = function(rows){
 	return table;
 }
 
-var serverIdJson = function(container, res, checked, stop){
-	checked += 1;
-		container.Config.Env.forEach(function(env){
-			env = env.split('=');
-			if ("VIRTUAL_HOST" === env[0]){
-				server = {"name": env[1], "id": data.Id};
-				servers.push(jsonToRow(server));
-			}
-		});
-		if (checked == stop){
-			res.end(rowsToTable(servers));
-			console.log(moment().toLocaleString() + '  ' + servers);
+var serverIdJson = function(container, servers, res, checked, stop){
+	checked.val += 1;
+	container.Config.Env.forEach(function(env){
+		env = env.split('=');
+		if ("VIRTUAL_HOST" === env[0]){
+			server = {"name": env[1], "id": container.Id};
+			servers.push(jsonToRow(server));
 		}
+	});
+	if (checked.val == stop){
+		res.end(rowsToTable(servers));
+		console.log(moment().toLocaleString() + '  ' + servers);
+	}
 }
 
 var allServers = function (res, containers){
 	var servers = [];
-	var checked = 0;
+	var checked = { "val": 0 };
 	containers.forEach(function (containerInfo){
 		docker.getContainer(containerInfo.Id).inspect(function(err, container){
-			serverIdJson(container, res, checked, stop);
+			serverIdJson(container, servers, res, checked, containers.length);
 		});
 	});
 }
